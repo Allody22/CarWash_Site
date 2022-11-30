3b49adba27f3a2cc57b620dee27d542b6731d3f5
Миша <m.bogdanov2@g.nsu.ru>
2022-11-30 22:59:35 +0700

    Логин теперь работает, регистрация работает Spring Security не подключен

---
+++ НАШ СЕРВЕР/javaServerForApp-main/src/main/java/com/example/induswithpostgresql/IndusWithPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.induswithpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example.induswithpostgresql")
+public class IndusWithPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndusWithPostgreSqlApplication.class, args);
+	}
+
+}
---
+++ НАШ СЕРВЕР/javaServerForApp-main/src/main/java/com/example/induswithpostgresql/model/orders/order.java
@@ -0,0 +1,4 @@
+package com.example.induswithpostgresql.model.orders;
+
+public class order {
+}
