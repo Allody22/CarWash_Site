b7ade118e946d56fa452dbaa23256fd30ab64255
Миша <m.bogdanov2@g.nsu.ru>
2023-02-27 20:59:00 +0700

    Что-то файлы для сборкин не отправились

---
+++ CarWash_Server/src/main/java/ru/nsu/carwash_server/CarWashServerApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.carwash_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWashServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarWashServerApplication.class, args);
+    }
+
+}
